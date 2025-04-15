7f6058103f893d4e6bb068c9769567cf7f913884
Joaquin-oc <joaquin.ocana@utec.edu.pe>
2025-04-15 17:35:11 -0500

    Añadiendo la función resta

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
 def calculate() -> float:
     pass
+
+
+def restar(a, b):
+    return a - b
+
+
+a = int(input("Pon el primer numero "))
+b = int(input("Pon el segundo numero "))
+print(restar(a, b))
